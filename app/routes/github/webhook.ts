4eddaf6e558e5eec8474c4b503a6722c904273b0
Massimo De Marchi <massimo@inkofpixel.com>
2021-12-16 15:49:01 +0100

    Update webhook.ts

--- app/routes/github/webhook.ts
+++ app/routes/github/webhook.ts
@@ -1,18 +1,33 @@
 import { ActionFunction, json } from "remix";
+import crypto from "crypto";
 
 export const action: ActionFunction = async ({ request }) => {
-  if (request.method === "POST") {
-    console.log("\nNEW GITHUB WEBHOOK!!");
-    const payload = await request.json();
-    console.log(payload);
-    return json(
-      {
-        message: "OK",
-        event: "",
-      },
-      200
-    );
-  } else {
-    return json({ message: "not supported" }, 400);
+  const GITHUB_WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET;
+  if (request.method !== "POST") {
+    return json({ message: "Method not allowed" }, 405);
   }
+  if (!GITHUB_WEBHOOK_SECRET) {
+    return json({ message: "GITHUB_WEBHOOK_SECRET not set" }, 500);
+  }
+  console.log("🪝 GITHUB WEBHOOK");
+  const signature = request.headers.get("X-Hub-Signature");
+  const generatedSignature = `sha1=${crypto
+    .createHmac("sha1", GITHUB_WEBHOOK_SECRET)
+    .update(JSON.stringify(request.body))
+    .digest("hex")}`;
+
+  if (signature !== generatedSignature) {
+    return json({ message: "Signature mismatch" }, 401);
+  }
+  const payload = await request.json();
+  const event = request.headers.get("X-GitHub-Event");
+  const response: any = {
+    message: `Webhook received: ${event}`,
+    event,
+  };
+  if (event === "push") {
+    response.branch = payload.ref.replace("refs/heads/", "");
+    response.pusher = payload.pusher.name;
+  }
+  return json(response, 200);
 };
