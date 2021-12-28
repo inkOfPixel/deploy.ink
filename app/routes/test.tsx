3e72b8c3f4a8d85b26052a3f6c23f695e9753a40
Massimo De Marchi <massimo@inkofpixel.com>
2021-12-28 20:05:36 +0100

    wip

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,8 +1,8 @@
 import { LoaderFunction } from "remix";
-import { PushQueue } from "~/jobs/push_job.server";
+import { PushJob } from "~/jobs/push_job.server";
 
 export const loader: LoaderFunction = async () => {
-  const result = await PushQueue.add({ branch: "jobs", cloneUrl: "" });
+  const result = await PushJob.performLater({ branch: "jobs", cloneUrl: "" });
   console.log("result", result.data);
   return result.data;
 };
