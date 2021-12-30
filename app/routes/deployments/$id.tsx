2ba71bf0288974b115d71a4c57583be5ccb4c213
Massimo De Marchi <massimo@inkofpixel.com>
2021-12-30 15:54:54 +0100

    improve deployment url

--- app/routes/deployments/$id.tsx
+++ app/routes/deployments/$id.tsx
@@ -74,6 +74,8 @@ export default function Job() {
   const processedOn = dayjs(job.processedOn);
   const finishedOn = dayjs(job.finishedOn);
 
+  const deploymentUrl = `https://${job.data.branch}.deploy.ink/admin`;
+
   return (
     <>
       <header>
@@ -124,7 +126,7 @@ export default function Job() {
               {finishedOn.isValid() && job.status === "completed" && (
                 <span className="hidden sm:block ml-3">
                   <a
-                    href={`https://${job.data.branch}.deploy.ink`}
+                    href={deploymentUrl}
                     className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     target="_blank"
                   >
@@ -160,7 +162,7 @@ export default function Job() {
                     <Menu.Item>
                       {({ active }) => (
                         <a
-                          href={`https://${job.data.branch}.deploy.ink`}
+                          href={deploymentUrl}
                           target="_blank"
                           className={classNames(
                             active ? "bg-gray-100" : "",
