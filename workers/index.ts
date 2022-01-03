f39a483a39d26440635fa90cc76079ccbfec5709
Massimo De Marchi <massimo@inkofpixel.com>
2022-01-04 00:29:39 +0100

    Add worker

--- workers/index.ts
+++ workers/index.ts
@@ -1,9 +1,11 @@
+import { DeleteDeploymentJob } from "~/jobs/delete_deployment_job";
 import { PushJob } from "../app/jobs/push_job.server";
 import { connection } from "../config/jobs";
 
 checkRedisConnection().then(() => {
   console.log("redis is ready");
   PushJob.startWorker({ connection });
+  DeleteDeploymentJob.startWorker({ connection });
 });
 
 function checkRedisConnection() {
